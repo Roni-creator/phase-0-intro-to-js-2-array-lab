8528e1e0c7824e008e2a2211eb6d6d28b3d1296f
veronicah <mbuviveronicah3@gmail.com>
2023-03-12 17:12:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution here!
+
+function destructivelyAppendCat(name)
+ { return cats.push(name);
+}
+
+
+function destructivelyPrependCat(name)
+ { return cats.unshift(name);
+}
+
+
+function destructivelyRemoveLastCat(name)
+ { return cats.pop(name);
+ }
+
+ 
+ 
+function destructivelyRemoveFirstCat(name)
+ { return cats.shift(name);
+ }
+
+ 
+function appendCat(name){
+    const newcats=[...cats];
+    newcats.push(name);
+return newcats;
+}
+
+
+function prependCat(name){
+    const newcats=[...cats];
+    newcats.unshift(name);
+    return newcats;
+}
+
+function removeLastCat(name){
+    const newcats=[...cats];
+    newcats.splice(name)
+    return newcats;
+}
+
+const cats=["Milo", "Otis", "Garfield"];
+function removeFirstCat(name){
+    const newcats=[...cats];
+    newcats.slice(name)
+    return newcats;
+}
